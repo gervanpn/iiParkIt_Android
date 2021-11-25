209ea9b55aee24b878157c1873613bf8a7919e29
burningman2k20 <60669658+burningman2k20@users.noreply.github.com>
2021-11-24 21:31:02 -0500

    adjustment

--- iiparkit/app/src/main/java/com/example/iiparkit/QRScannerActivity.kt
+++ iiparkit/app/src/main/java/com/example/iiparkit/QRScannerActivity.kt
@@ -5,19 +5,26 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Base64
 import android.util.Log
+import android.view.ContentInfo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.drawToBitmap
 import androidx.lifecycle.LifecycleOwner
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
@@ -41,10 +48,14 @@ class QRScannerActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
     private val client = OkHttpClient()
 
+    lateinit var text : TextView
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrscanner)
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        text = findViewById(R.id.textView2)
 
         codeScanner = CodeScanner(this, scannerView)
 
@@ -60,6 +71,23 @@ class QRScannerActivity : AppCompatActivity() {
         // Callbacks
         codeScanner.decodeCallback = DecodeCallback {
             runOnUiThread {
+                var bytes = codeScanner to byteArrayOf()
+                    //.camera.toDrawable().toBitmap() to byteArrayOf()
+//                var bytes = scannerView.onReceiveContent( payload, {
+//
+//                }
+//
+//                )
+
+
+
+                //drawToBitmap() to byteArrayOf()
+                //var bytes = scannerView.drawToBitmap() to byteArrayOf()
+                Log.d("bytes", bytes.second.size.toString())
+//                val job = GlobalScope.async {saveBitmap("","scan.png",bytes.second) }
+//                runBlocking {
+//                    job.await()
+//                }
                 Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
             }
         }
@@ -99,10 +127,11 @@ class QRScannerActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
 
                 //val collectionType: Type = object : TypeToken<DataParsing>() {}.getType()
-                val body = response.body?.bytes()
+                val body = response.body
+                text.text = body?.toString()
                 if (body != null) {
                     //Log.d("response", body.toString())
-                    val job1 = GlobalScope.async {saveBitmap("","test.png",body) }
+                    val job1 = GlobalScope.async {saveBitmap("","test.png",body.bytes()) }
                     runBlocking {
                         job1.await()
                     }
@@ -132,7 +161,6 @@ class QRScannerActivity : AppCompatActivity() {
         //file.printWriter().use {out -> out.print(bitmap)}
 
             file.outputStream().use {
-
             image.compress(Bitmap.CompressFormat.PNG, 100, it)
         }
     }
